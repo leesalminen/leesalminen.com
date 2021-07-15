5b777ef0ea0ffd98fa7c61effe90518cff345caa
Lee Salminen <leesalminen@gmail.com>
2021-07-15 12:11:35 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import Terminal from 'terminal-in-react';
+import pseudoFileSystemPlugin from 'terminal-in-react-pseudo-file-system-plugin';
+import OnMountPlugin from './OnMountPlugin';
+
+const FileSystemPlugin = pseudoFileSystemPlugin();
+
+const showMsg = () => 'Hello World'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Terminal
+      plugins={[
+        FileSystemPlugin,
+        OnMountPlugin
+      ]}
+      hideTopBar={true}
+      allowTabs={false}
+      startState='maximised'
+      color='green'
+      backgroundColor='black'
+      barColor='black'
+      style={{ fontWeight: "bold", fontSize: "1em" }}
+      commands={{
+        'open-google': () => window.open('https://www.google.com/', '_blank'),
+        showmsg: showMsg,
+        popup: () => alert('Terminal in React')
+      }}
+      descriptions={{
+        'open-google': 'opens google.com',
+        showmsg: 'shows a message',
+        alert: 'alert', 
+        popup: 'alert',
+      }}
+      msg={`Hi there, I'm Lee Salminen! Parker is my boy, and Nikki is my ol' lady.`}
+    />
   );
 }
 
---
+++ src/OnMountPlugin.js
@@ -0,0 +1,30 @@
+import PluginBase from 'terminal-in-react/lib/js/components/Plugin';
+
+export default class OnMountPlugin extends PluginBase {
+  static displayName = 'OnMountPlugin';
+  static version = '1.0.0';
+
+  constructor(api, config) {
+    super(api, config);
+
+    setTimeout(() => {
+      const createDir = this.api.getPluginMethod('PseudoFileSystem', 'createDir')
+      const removeDir = this.api.getPluginMethod('PseudoFileSystem', 'removeDir')
+      const parsePath = this.api.getPluginMethod('PseudoFileSystem', 'parsePath')
+      const createFile = this.api.getPluginMethod('PseudoFileSystem', 'writeFile')
+      
+      removeDir(parsePath('/home/user'))
+      createDir(parsePath('/home/lee'))
+      createDir(parsePath('/home/parker'))
+      createDir(parsePath('/home/nikki'))
+      this.api.runCommand('cd /home');
+      this.api.runCommand('touch /home/lee/README')
+      this.api.runCommand('touch /home/nikki/README')
+      this.api.runCommand('touch /home/parker/README')
+      this.api.runCommand('echo 42 >> /home/lee/README')
+      this.api.runCommand('echo hi >> /home/nikki/README')
+      this.api.runCommand('echo yo >> /home/parker/README')
+      
+    }, 0)
+  }
+}
